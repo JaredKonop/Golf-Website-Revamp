cabbba8d1101fc5e127fecf88c607e81f2244a1f
JaredKonop <wajared23@gmail.com>
2024-06-10 18:43:34 -0400

    Updated Home page

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -14,16 +14,16 @@ const Home: React.FC = () => {
     <div>
       <div>
         <h1>Clover Hill Golf Course</h1>
-        <div className={styles.slogan}>
-          <p>
-            From Tee to Green: Crafting Memorable Moments, One Swing at a Time
-          </p>
-        </div>
       </div>
       <BusinessHours />
       <button className={styles.button} onClick={handleButtonClick}>
         Book a Tee Time
       </button>
+      <div className={styles.slogan}>
+        <p>
+          From Tee to Green: Crafting Memorable Moments, One Swing at a Time
+        </p>
+      </div>
     </div>
   );
 };
